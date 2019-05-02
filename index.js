504710f6ef39babbb334fd3df3bf8089bef33a21
Luke Pinto <lap5508@gmail.com>
2019-05-01 20:00:24 -0400

    Adding second endpoint

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get('/', (req, res) => {
    res.send('Hello World!');
 });
 
+app.get('/friends', (req, res) => {
+   res.send('Luke and Dustin');
+});
+
 app.listen(3000, () => {
    console.log('listening on 3000!');
 });
